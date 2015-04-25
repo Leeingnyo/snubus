9407901fb08df527cd66d8f7d36d3b2b9116d0c7
yeonseok-yi <yeonseok-yi@users.noreply.github.com>
2015-04-25 13:38:52 +0000

    Add Bus.validate

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,2 +1,26 @@
 class Bus < ActiveRecord::Base
+  def self.validate
+    if valid?
+      return
+    end
+
+    update
+  end
+
+  private
+
+  UPDATE_CYCLE = 30
+
+  @@last_update_time = nil
+
+  def self.valid?
+    return !@@last_update_time.nil? &&
+           Time.now - @@last_update_time < UPDATE_CYCLE
+  end
+
+  def self.update
+    #TODO: Remove all entries and add new entries.
+
+    @@last_update_time = Time.now
+  end
 end
