a467a5a7c6115ac1023b2ede4cc7a38d61204c76
Backspe <bschoe@gmail.com>
2015-04-25 17:08:26 +0900

    implements self.get_duration()

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,2 +1,16 @@
 class Edge < ActiveRecord::Base
+  def self.get_duration(line, start_stop, end_stop)
+    @time = 0
+    @iter_stop = start_stop
+    @edge = Edge.find_by (line_id: line, start_stop_id: start_stop)
+    while @edge do
+      @time += @edge[:time]
+      @iter_stop = @edge[:end_stop_id]
+      if @iter_stop == end_stop
+        return @time
+      end
+      @edge = Edge.find_by (line_id: line, start_stop_id: iter_stop)
+    end
+    return -1
+  end
 end
