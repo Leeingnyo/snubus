134f339dfc0cebe508eabef5698751d00a94fbc8
Backspe <bschoe@gmail.com>
2015-04-26 01:09:22 +0900

    implement routes#show_route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,4 +1,23 @@
 class RoutesController < ApplicationController
   def show_routes
+    lines = Line.all
+    @routes = [];
+    lines.each do |line|
+      time = Edge.get_duration(line[:line_id], params[:departure], params[:destination])
+      if time > -1
+        r = SubRoute.new(from = params[:departure], to = params[:destination], line = line, moving = time, waiting = 0)
+        @routes.push(r)
+      end
+    end
+  end
+end
+
+class SubRoute
+  def initialize(from, to, line, moving, waiting)
+    @from = from
+    @to = to
+    @line = line
+    @moving = moving
+    @waiting = waiting
   end
 end
