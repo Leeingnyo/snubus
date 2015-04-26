e6b61151c9e1c0d826bb149c09450e92b2950f5d
GaeNari <gaenari31@gmail.com>
2015-04-26 20:04:59 +0900

    modify [0] to .first

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -3,7 +3,7 @@ class StopsController < ApplicationController
     @results = Array.new
     @stop = Stop.find_by :stop_id => params[:id]
     Edge.where(:to => @stop.stop_id).find_each do |e|
-      coming = Bus.coming_buses(e)[0]
+      coming = Bus.coming_buses(e).first
       if coming != nil
         pos = Edge.find coming["edge_id"]
         time = Edge.get_duration(coming["line_id"], pos.to, e.to)
