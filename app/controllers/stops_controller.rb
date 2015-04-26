21b3832d148517e33069784bb1588710484a73e8
GaeNari <gaenari31@gmail.com>
2015-04-26 18:27:56 +0900

    fix stops_controller, apply schema change to app/models/edge.rb

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,12 +1,15 @@
 class StopsController < ApplicationController
   def show
     @results = Array.new
-    stop = Stop.find_by stop_id: params[:id]
-    Edge.where(end_stop_id: stop.stop_id).find_each do |e|
+    stop = Stop.find_by :stop_id => params[:id]
+    Edge.where(:to => stop.stop_id).find_each do |e|
       coming = Bus.coming_buses(e)[0]
-      time = Edge.get_duration(coming.line.line_id, coming.edge.end_stop_id, e.end_stop_id)
-      time += coming.time
-      @results.push({line: coming.line, time: time})
+      if coming != nil
+        pos = Edge.find coming["edge_id"]
+        time = Edge.get_duration(coming["line_id"], pos.to, e.to)
+        time += coming["time"]
+        @results.push({:line => coming["line_id"], :time => time})
+      end
     end
   end
 end
