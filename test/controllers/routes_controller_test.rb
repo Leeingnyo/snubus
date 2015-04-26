20fb33f9df3530fcff83c5fb7920b33e82ad2acc
GaeNari <gaenari31@gmail.com>
2015-04-27 00:21:23 +0900

    some controller tests, add coverage/* to .gitignore

--- test/controllers/routes_controller_test.rb
+++ test/controllers/routes_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class RoutesControllerTest < ActionController::TestCase
-  test "should get show_routes" do
-    get :show_routes
+  test "#should_get_show_routes" do
+    get :show_routes, {:departure => 33712, :destination => 3447}
     assert_response :success
   end
-
 end
--- test/controllers/stops_controller_test.rb
+++ test/controllers/stops_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StopsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#stop_success" do
+    get :show, {:id => "33749"}
+    assert_match '5511', response.body
+  end
+  
+  test "#stop_failure" do
+    get :show, {:id => "99999"}
+    assert_no_match '5511', response.body
+  end
 end
