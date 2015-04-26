6c693dde26be3409f3cda2e02455c807bb76d888
Backspe <bschoe@gmail.com>
2015-04-26 21:33:28 +0900

    improve show_routes
    bug fix in routes_controller
    prettify view show_routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,11 +1,16 @@
 class RoutesController < ApplicationController
   def show_routes
     lines = Line.all
+    @from = Stop.find_by(:stop_id => params[:departure]).name
+    @to = Stop.find_by(:stop_id => params[:destination]).name
     @routes = [];
     lines.each do |line|
       time = Edge.get_duration(line[:line_id], params[:departure], params[:destination])
       if time > -1
-        r = SubRoute.new(from = params[:departure], to = params[:destination], line = line, moving = time, waiting = 0)
+        r = SubRoute.new(
+          from = Stop.find_by(:stop_id => params[:departure]),
+          to = Stop.find_by(:stop_id => params[:destination]),
+          line = line, moving = time, waiting = 0)
         @routes.push(r)
       end
     end
@@ -13,6 +18,7 @@ class RoutesController < ApplicationController
 end
 
 class SubRoute
+  attr_reader :from, :to, :line, :moving, :waiting
   def initialize(from, to, line, moving, waiting)
     @from = from
     @to = to
