083ada09bc3ddb22b2a17d6263eb65e00c14834d
yeonseok-yi <yeonseok-yi@users.noreply.github.com>
2015-04-26 08:12:40 +0000

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "AllBuses" => "buses#all_buses"
+  post "/AllBuses" => "buses#all_buses"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
