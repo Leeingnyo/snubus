a984214724d35ceae682371eb40b570d16ab357c
Backspe <bschoe@gmail.com>
2015-05-12 22:29:28 +0900

    use seed

--- test/models/edge_test.rb
+++ test/models/edge_test.rb
@@ -2,31 +2,21 @@ require 'test_helper'
 
 class EdgeTest < ActiveSupport::TestCase
   test '#get_duration' do
+    seed
     t = Edge.get_duration('4551100', '33712', '3447')
     assert_equal(1080, t)
-  end
-
-  test '#get_duration when same stop' do
     t = Edge.get_duration('4551100', '33712', '33712')
     assert_equal(0, t)
-  end
-
-  test '#get_duration when no route' do
     t = Edge.get_duration('4551100', '3447', '33712')
     assert_equal(-1, t)
   end
 
   test '#get_stop_list' do
+    seed
     t = Edge.get_stop_list('4551100', '33712', '3447')
     assert_equal(["33712", "33749", "3447"], t)
-  end
-
-  test '#get_stop_list when same stop' do
     t = Edge.get_stop_list('4551100', '33712', '33712')
     assert_equal(["33712"], t)
-  end
-
-  test '#get_stop_list when no route' do
     t = Edge.get_stop_list('4551100', '3447', '33712')
     assert_equal(nil, t)
   end
