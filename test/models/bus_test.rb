f54cdcd06db3ef30702d9bcee12fea01bd8201ca
yeonseok-yi <yeonseok-yi@users.noreply.github.com>
2015-05-12 06:02:39 +0000

    Fix array index error

--- test/models/bus_test.rb
+++ test/models/bus_test.rb
@@ -4,6 +4,11 @@ class BusTest < ActiveSupport::TestCase
   test '#coming_buses' do
     e = Edge.find(2)
     res = Bus.coming_buses(e)
+
+    if res.empty?
+      return
+    end
+
     assert_equal(res.fetch(0)["time"], 180)
     assert_equal(res.fetch(1)["time"], 60)
   end
