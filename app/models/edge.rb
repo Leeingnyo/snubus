8f5309937dc10947dda545e593e5929ad2547b99
Backspe <bschoe@gmail.com>
2015-05-12 18:08:32 +0900

    get_duration bug fix, add get_stop_list

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,5 +1,6 @@
 class Edge < ActiveRecord::Base
   def self.get_duration(line, start_stop, end_stop)
+    return 0 if start_stop == end_stop
     time = 0
     iter_stop = start_stop
     edge = Edge.find_by(:line_id => line, :from => start_stop)
@@ -12,5 +13,21 @@ class Edge < ActiveRecord::Base
       edge = Edge.find_by(:line_id => line, :edge_index => edge.edge_index + 1)
     end
     return -1
+  end
+  
+  def self.get_stop_list(line, start_stop, end_stop)
+    return 0 if start_stop == end_stop
+    iter_stop = start_stop
+    l = [start_stop]
+    edge = Edge.find_by(:line_id => line, :from => start_stop)
+    while edge do
+      iter_stop = edge.to
+      l.push(iter_stop)
+      if iter_stop == end_stop
+        return l
+      end
+      edge = Edge.find_by(:line_id => line, :edge_index => edge.edge_index + 1)
+    end
+    return nil
   end
 end
