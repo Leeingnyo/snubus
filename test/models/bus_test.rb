86fb450b92f58332278b8192a2299720720d27d8
GaeNari <gaenari31@gmail.com>
2015-05-12 22:16:14 +0900

    Test for Bus.update

--- test/models/bus_test.rb
+++ test/models/bus_test.rb
@@ -2,14 +2,24 @@ require 'test_helper'
 
 class BusTest < ActiveSupport::TestCase
   test '#coming_buses' do
+    seed
     e = Edge.find(2)
     res = Bus.coming_buses(e)
-
-    if res.empty?
-      return
-    end
-
+    
+    assert res
     assert_equal(res.fetch(0)["time"], 60)
     assert_equal(res.fetch(1)["time"], 60)
+  end
+
+  test '#update' do
+    seed
+    Bus.destroy_all
+    bus_before = Bus.first
+    assert_not bus_before
+    Rails.configuration.x.should_not_update_buses = false
+    Bus.validate
+    bus_after = Bus.first
+    Rails.configuration.x.should_not_update_buses = true
+    assert bus_after
   end
 end
