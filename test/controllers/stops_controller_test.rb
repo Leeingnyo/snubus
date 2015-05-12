be089ff1df2d0cd0006bc536ec74b2d765ffa630
Leeingnyo <dyd1928@naver.com>
2015-05-12 14:22:37 +0900

    erase test that was available in iteration1

--- test/controllers/stops_controller_test.rb
+++ test/controllers/stops_controller_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class StopsControllerTest < ActionController::TestCase
-  test "#stop_success" do
-    get :show, {:id => "33749"}
-    assert_match '5511', response.body
-  end
 end
