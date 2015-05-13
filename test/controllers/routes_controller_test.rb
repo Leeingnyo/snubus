d76b2c4e582f2bc5d3afc9a7dac371a9d6dec4db
Backspe <bschoe@gmail.com>
2015-05-13 14:33:03 +0900

    test routes#show_routes

--- test/controllers/routes_controller_test.rb
+++ test/controllers/routes_controller_test.rb
@@ -4,5 +4,8 @@ class RoutesControllerTest < ActionController::TestCase
   test "#should_get_show_routes" do
     get :show_routes, {:departure => 33712, :destination => 3447}
     assert_response :success
+
+    get :show_routes, {:departure => 33712, :destination => 34400}
+    assert_redirected_to :root
   end
 end
