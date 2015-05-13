776875ae3012c5543b96ed5cb58a37edec543dd6
Backspe <bschoe@gmail.com>
2015-05-13 11:37:48 +0900

    edge test for test db

--- test/models/edge_test.rb
+++ test/models/edge_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class EdgeTest < ActiveSupport::TestCase
   test '#get_duration' do
-    seed
     t = Edge.get_duration('4551100', '33712', '3447')
     assert_equal(1080, t)
     t = Edge.get_duration('4551100', '33712', '33712')
@@ -12,9 +11,8 @@ class EdgeTest < ActiveSupport::TestCase
   end
 
   test '#get_stop_list' do
-    seed
-    t = Edge.get_stop_list('4551100', '33712', '3447')
-    assert_equal(["33712", "33749", "3447"], t)
+    t = Edge.get_stop_list('4551100', '34733', '34732')
+    assert_equal(["34733", "33748", "33749", "33750", "34732"], t)
     t = Edge.get_stop_list('4551100', '33712', '33712')
     assert_equal(["33712"], t)
     t = Edge.get_stop_list('4551100', '3447', '33712')
