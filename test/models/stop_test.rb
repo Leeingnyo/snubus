70e027e0a7a9de1bea1c90f1134ff90bde2240be
yeonseok-yi <yeonseok-yi@users.noreply.github.com>
2015-05-13 04:44:11 +0000

    Add test for checking validity of stops

--- test/models/stop_test.rb
+++ test/models/stop_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class StopTest < ActiveSupport::TestCase
+  test "Check stops validity" do
+    tests = [{:id => "33712", :name => "제2공학관"},
+             {:id => "33707", :name => "경영대"},
+             {:id => "33353", :name => "법대입구"},
+             {:id => "33351", :name => "공대입구"},
+             {:id => "34733", :name => "에너지자원연구소"},
+             {:id => "33354", :name => "신소재"}]
+
+    tests.each do |test|
+      stop = Stop.find_by(:stop_id => test[:id])
+      assert(stop, test[:name])
+      assert_equal(stop.name, test[:name], test[:name])
+    end
+  end
 end
