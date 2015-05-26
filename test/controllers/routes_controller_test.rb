bd97ff305c9b95a3ef71cc47f52d47a2f70dbccb
Leeingnyo <dyd1928@naver.com>
2015-05-26 21:53:19 +0900

    test subway

--- test/controllers/routes_controller_test.rb
+++ test/controllers/routes_controller_test.rb
@@ -13,4 +13,16 @@ class RoutesControllerTest < ActionController::TestCase
     get :last_routes
     assert_response :success
   end
+
+  test "#show_routes_with_subway" do
+    get :show_routes, {:departure => 33712, :destination => 3719}
+    assert_match /지하철/, response.body
+  end
+
+  test "#show_routes_to_sadang" do
+    get :show_routes, {:departure => 33712, :destination => 10}
+    assert_match /사당방면/, response.body
+    assert_match /서울대입구역/, response.body
+    assert_match /낙성대입구/, response.body
+  end
 end
