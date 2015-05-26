79583d4b6f29437cd55bc6e43cf887d2f7282304
Leeingnyo <dyd1928@naver.com>
2015-05-26 21:25:48 +0900

    get subway time table when user select station

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -13,14 +13,19 @@ class RoutesController < ApplicationController
 
     @from = from_stop.name
     @to = to_stop.name
-    @routes = find_route(from_stop, to_stop, 0)
+    if to_stop.property == "stop"
+      @routes = find_route(from_stop, to_stop, 0)
+    elsif
+      @routes = find_route(from_stop, Stop.find_by(:stop_id => "3719"), 0)
+      @routes += find_route(from_stop, Stop.find_by(:stop_id => "10422"), 0)
+    end
     @routes.each do |route|
       if route.to.stop_id == "3719" || route.to.stop_id == "10422"
         now = Time.now
         arrive = now + route.time
         today = Time.new(now.year, now.month, now.day)
         time = (arrive - today) / 60
-        station_name = params[:destination] == "3719" ? "서울대입구역" : "낙성대역"
+        station_name = route.to.stop_id == "3719" ? "서울대입구역" : "낙성대역"
         daytype = today.saturday? ? 1 : today.sunday? ? 2 : 0
         route.subway_to_sadang = Station.where(:name => station_name, :direction => "사당", :kind => daytype).where("arrival_time > #{time}").order("arrival_time asc").first
         route.subway_to_bongcheon = Station.where(:name => station_name, :direction => "봉천", :kind => daytype).where("arrival_time > #{time}").order("arrival_time asc").first
